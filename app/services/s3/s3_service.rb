03db8eaeb76aa5ce9511d5d6369ba4c1a2977a66
Thierry Scotto <thierry_192@outlook.com>
2021-08-11 23:29:32 -0300

    Add service s3 - added function to connect to the s3

--- app/services/s3/s3_service.rb
+++ app/services/s3/s3_service.rb
@@ -5,12 +5,8 @@ Dotenv.load
 
 module AppServices
   class S3
-    def initialize(bucketName=nil)
-      s3 = Aws::S3::Resource.new
-      @bucket = s3.bucket(ENV['BUCKET_NAME'])
-    end
-
     def self.getFiles
+      getConnection
       data = @bucket.objects.each do |obj|
         "#{obj.key} => #{obj.etag}"
       end
@@ -18,11 +14,16 @@ module AppServices
     end
 
     def self.getFile(fileName)
+      getConnection
       @bucket.objects(prefix: fileName)
     end
 
-    def update
-      puts "test"
-    end
+    private
+      def self.getConnection
+        s3 = Aws::S3::Resource.new
+        @bucket = s3.bucket(ENV['BUCKET_NAME'])
+      end
   end
 end
+
+AppServices::S3.getFile('test')
